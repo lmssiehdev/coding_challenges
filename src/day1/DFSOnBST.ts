b78bce6a43aa0a3446af0d2125b921fcf55796c6
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-07-31 16:38:55 +0100

    solve breath first search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,13 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function walk(node: BinaryNode<number> | null, needle: number): boolean {
+    if (!node) return false;
 
+    if (node.value === needle) return true;
+
+    if (node.value < needle) return walk(node.right, needle);
+
+    return walk(node.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head, needle);
 }
